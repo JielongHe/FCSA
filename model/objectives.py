734442dfc8f7659b0abdf31c23a3aa6cf8ddf294
JielongHe <jielonghe9@gmail.com>
2025-09-20 23:36:26 +0800

    Update objectives.py

--- model/objectives.py
+++ model/objectives.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-def compute_sdm(epoch, image_fetures, text_fetures, pid, logit_scale,  args = None, image_id=None, factor=0.3, epsilon=1e-8):
+def compute_cmpm(epoch, image_fetures, text_fetures, pid, logit_scale,  args = None, image_id=None, factor=0.3, epsilon=1e-8):
 
     # print(image_fetures)
     # print(text_fetures)
@@ -141,39 +141,4 @@ def compute_id(image_logits, text_logits, labels):
     
     return loss / 2
 
-
-def compute_cmpm(image_embeddings, text_embeddings, labels, epsilon=1e-8):
-    """
-    Cross-Modal Projection Matching Loss(CMPM)
-    :param image_embeddings: Tensor with dtype torch.float32
-    :param text_embeddings: Tensor with dtype torch.float32
-    :param labels: Tensor with dtype torch.int32
-    :return:
-        i2t_loss: cmpm loss for image projected to text
-        t2i_loss: cmpm loss for text projected to image
-        pos_avg_sim: average cosine-similarity for positive pairs
-        neg_avg_sim: averate cosine-similarity for negative pairs
-    """
-
-    batch_size = image_embeddings.shape[0]
-    labels_reshape = torch.reshape(labels, (batch_size, 1))
-    labels_dist = labels_reshape - labels_reshape.t()
-    labels_mask = (labels_dist == 0).float()
-
-    image_norm = image_embeddings / image_embeddings.norm(dim=1, keepdim=True)
-    text_norm = text_embeddings / text_embeddings.norm(dim=1, keepdim=True)
-    image_proj_text = torch.matmul(image_embeddings, text_norm.t())
-    text_proj_image = torch.matmul(text_embeddings, image_norm.t())
-
-    # normalize the true matching distribution
-    labels_mask_norm = labels_mask / labels_mask.norm(dim=1)
-
-    i2t_pred = F.softmax(image_proj_text, dim=1)
-    i2t_loss = i2t_pred * (F.log_softmax(image_proj_text, dim=1) - torch.log(labels_mask_norm + epsilon))
-    t2i_pred = F.softmax(text_proj_image, dim=1)
-    t2i_loss = t2i_pred * (F.log_softmax(text_proj_image, dim=1) - torch.log(labels_mask_norm + epsilon))
-
-    cmpm_loss = torch.mean(torch.sum(i2t_loss, dim=1)) + torch.mean(torch.sum(t2i_loss, dim=1))
-
-    return cmpm_loss
 
